a5f89c8ed272649cc0c0ad962f0c31e514269eb4
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-07-15 16:52:23 -0600

    cambios

--- proyecto1.js
+++ proyecto1.js
@@ -1,12 +1,28 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-axios.get('http://alumchat.xyz')
-  .then(response => {
-    console.log(response.data);
-    console.log("No hubo errores")
-    // Aquí puedes manipular los datos recibidos
-  })
-  .catch(error => {
-    console.log(error);
-    // Manejo de errores
-  });
+// axios.get('http://alumchat.xyz')
+//   .then(response => {
+//     console.log(response.data);
+//     console.log("No hubo errores")
+//     // Aquí puedes manipular los datos recibidos
+//   })
+//   .catch(error => {
+//     console.log(error);
+//     // Manejo de errores
+//   });
+
+const io = require('socket.io-client');
+const serverUrl = "http://alumchat.xyz:5222"
+const socket = io(serverUrl);
+
+// Realizar el register al servidor.
+// Se tiene que mandar usuario y contraseña.
+// Si el usuario ya existe, se debe de mandar un error.
+
+user = "val20159"
+password = "1234"
+
+socket.on('Register', {user, password}, (response) => {
+  console.log("Datos")
+    console.log(response)
+})
