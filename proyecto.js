604aacb1407f9f651c713d48ed28cdab5c369af3
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-07-29 18:27:39 -0600

    cambios

--- proyecto.js
+++ proyecto.js
@@ -1,36 +1,189 @@
 const { client, xml } = require("@xmpp/client");
 const debug = require("@xmpp/debug");
+const { unsubscribe } = require("diagnostics_channel");
+const net = require("net");
+const cliente = new net.Socket();
 
-const xmpp = client({
-  service: "xmpp://alumchat.xyz:5222",
-  domain: "alumchat.xyz",
-  username: "val20159",
-  password: "1234",
+// Password: 1234
+
+const readline = require("readline");
+
+// const xmpp = client({
+//   service: "xmpp://alumchat.xyz:5222",
+//   domain: "alumchat.xyz",
+//   username: "val20159",
+//   password: "1234",
+// });
+
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+// // debug(xmpp, true);
+
+// xmpp.on("error", (err) => {
+//   console.error(err);
+// });
+
+// xmpp.on("online", async (address) => {
+//   // Makes itself available
+//   await xmpp.send(xml("presence"));
+
+//   // Sends a chat message to "gon20362@alumchat.xyz"
+//   const message = xml(
+//     "message",
+//     { type: "chat", to: "mom20067@alumchat.xyz" },
+//     xml("body", {}, "Hello, this is a message from val20159!"),
+//   );
+
+//   // Log the sent message to avoid an infinite loop of receiving it as well
+//   //console.log("Sending message:", message.toString());
+
+//   await xmpp.send(message);
+// });
+
+// xmpp.start().catch(console.error);
+
+// Lector del input.
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// Menú inicial.
+function showMenu() {
+  console.log("\nMenú");
+  console.log("1. Iniciar sesión");
+  console.log("2. Registrarse");
+  console.log("3. Salir");
 
-// debug(xmpp, true);
+  rl.question("Selecciona una opción: ", (answer) => {
+    switch (answer) {
+      case "1":
+        // Pidiendo los datos del usuario, o sea usuario y contraseña.
+        
+        rl.question("Usuario: ", (username) => {
+          rl.question("Contraseña: ", (password) => {
+            login(username, password);
+        
+          })})
+        break;
+      case "2":
+        register();
+        break;
+      case "3":
+        console.log("Saliendo...");
+        rl.close(); // Cerrar la interfaz antes de salir del programa.
+        showMenu()
+        break;
+      default:
+        console.log("Opción inválida.");
+        showMenu();
+    }
+  });
+}
 
-xmpp.on("error", (err) => {
-  console.error(err);
-});
+function login(username, password) {
+  // Implementa aquí la lógica para iniciar sesión.
+  // Por ejemplo, puedes pedir al usuario su nombre de usuario y contraseña y validarlas.
 
-xmpp.on("online", async (address) => {
-  // Makes itself available
-  await xmpp.send(xml("presence"));
+  //console.log("Usuario: ", username)
+  //console.log("Contraseña:", password)
 
-  // Sends a chat message to "gon20362@alumchat.xyz"
-  const message = xml(
-    "message",
-    { type: "chat", to: "gon20362@alumchat.xyz" },
-    xml("body", {}, "Hello, this is a message from val20159!"),
-  );
+  const xmpp = client({
+    service: "xmpp://alumchat.xyz:5222",
+    domain: "alumchat.xyz",
+    username: username,
+    password: password,
+    terminal: true,
+  });
 
-  // Log the sent message to avoid an infinite loop of receiving it as well
-  //console.log("Sending message:", message.toString());
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-  await xmpp.send(message);
-});
+  // debug(xmpp, true);
 
-xmpp.start().catch(console.error);
+  xmpp.on("error", (err) => {
+    console.error(err);
+  });
+
+  xmpp.on("online", async (address) => {
+    // Makes itself available
+    await xmpp.send(xml("presence"));
+
+    console.log("Inició sesión con este address: ", address)
+
+    // const changePasswordRequest = xml(
+    //   "iq",
+    //   { type: "set", id: "change_password" },
+    //   xml(
+    //     "query",
+    //     { xmlns: "jabber:iq:register" },
+    //     xml("username", {}, "your_username"),
+    //     xml("password", {}, "170301M@rzo")
+    //   )
+    // );
+
+    // xmpp.send(changePasswordRequest).then((response) => {
+    //   // Procesar la respuesta del servidor, que debería indicar si el cambio de contraseña fue exitoso o no.
+    //   console.log("Contraseña cambiada exitosamente.");
+    // }).catch((error) => {
+    //   console.error("Error al cambiar la contraseña:", error);
+    // });
+
+    // // Sends a chat message to "gon20362@alumchat.xyz"
+    // const message = xml(
+    //   "message",
+    //   { type: "chat", to: "mom20067@alumchat.xyz" },
+    //   xml("body", {}, "Hello, this is a message from val20159!"),
+    // );
+
+    // // Log the sent message to avoid an infinite loop of receiving it as well
+    // //console.log("Sending message:", message.toString());
+
+    // await xmpp.send(message);
+  });
+
+  xmpp.start().catch(console.error);
+}
+
+function register() {
+  cliente.connect(5222, 'alumchat.xyz', function() {
+      //console.log('Conectado al servidor XMPP');
+      cliente.write("<stream:stream to='alumchat.xyz' xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' version='1.0'>");
+  });
+
+
+  // Pidiendo al usuario los datos.
+  rl.question("Usuario: ", (username) => {
+    rl.question("Contraseña: ", (password) => {
+      cliente.on('data', function(data) {
+          if (data.toString().includes('<stream:features>')) {
+              // Enviar consulta de registro
+              const xmlRegister = `
+              <iq type="set" id="reg_1" mechanism='PLAIN'>
+                <query xmlns="jabber:iq:register">
+                  <username>${username}</username>
+                  <password>${password}</password>
+                </query>
+              </iq>
+              `;
+              cliente.write(xmlRegister);
+          } else if (data.toString().includes('<iq type="result"')) {
+              // Registro exitoso
+              console.log('Registro exitoso');
+              showMenu();
+          } else if (data.toString().includes('<iq type="error"')) {
+              // Error al registrar
+              console.log('Error al registrar', data.toString());
+          }
+      });
+    });
+  });
+
+
+  cliente.on('close', function() {
+      console.log('Conexión cerrada');
+  });
+}
+
+
+// Mostrar el menú inicial.
+showMenu();
